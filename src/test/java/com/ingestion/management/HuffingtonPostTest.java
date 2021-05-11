d1fc095c37edd8dae663f4fcfcf3c3a8b8fb524c
Geo2000 <54718571+Geo2000@users.noreply.github.com>
2021-05-11 16:52:44 +0300

    HuffingtonPostTest added

---
+++ src/test/java/com/ingestion/management/HuffingtonPostTest.java
@@ -0,0 +1,20 @@
+package com.ingestion.management;
+
+import com.ingestion.management.business.HuffingtonPost;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+public class HuffingtonPostTest {
+    private HuffingtonPost huffingtonPost = new HuffingtonPost();
+
+    @Test
+    public void shouldScrapWithoutErrors() {
+        assertThatCode(() -> huffingtonPost.scrapMainPage()).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void scrapWithoutErrorsPageContent() {
+        assertThatCode(() -> huffingtonPost.scrapPageContent("https://www.huffpost.com/entry/fda-approves-pfizer-vaccine-12-to-15-year-olds_n_6070be53c5b6616dcd780ef5")).doesNotThrowAnyException();
+    }
+}
