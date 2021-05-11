2f107b510a37ba381b8499848e5642a80a6e2ff9
Geo2000 <54718571+Geo2000@users.noreply.github.com>
2021-05-11 17:02:43 +0300

    Added some tests for NewsControllerTest.java

--- src/test/java/com/ingestion/management/NewsControllerTest.java
+++ src/test/java/com/ingestion/management/NewsControllerTest.java
@@ -16,10 +16,13 @@ import org.springframework.http.ResponseEntity;
 import com.ingestion.management.model.News;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -38,7 +41,7 @@ public class NewsControllerTest {
     @Test
     public void shouldReturnBadRequestWhenPassingNullToCreate() {
         ResponseEntity<News> obj = controller.create(null);
-        assertEquals(obj.getStatusCode().value(), 400);
+        assertEquals(400, obj.getStatusCode().value() );
     }
 
     @Test
@@ -55,7 +58,50 @@ public class NewsControllerTest {
         news.setThumbnail("");
         when(repo.save(news)).thenReturn(news);
         ResponseEntity<News> obj = controller.create(news);
-        assertEquals(obj.getStatusCode().value(), 201);
+        assertEquals(201, obj.getStatusCode().value());
     }
 
+    @Test
+    public void shouldReturnNewsSources() {
+        repo = mock(NewsRepository.class);
+        controller = new NewsController(repo, newsService);
+
+        ResponseEntity<List<String> > sources = controller.getNewsSources();
+
+        assertEquals(200, sources.getStatusCode().value());
+        assertEquals(4, sources.getBody().size());
+    }
+
+    @Test
+    public void shouldWorkWithoutException() {
+        repo = mock(NewsRepository.class);
+        controller = new NewsController(repo, newsService);
+
+        assertThatCode(() -> controller.callNews()).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void shouldReturnNotFound() {
+        repo = mock(NewsRepository.class);
+        controller = new NewsController(repo, newsService);
+
+        when(repo.findById(any())).thenReturn(Optional.empty());
+
+        ResponseEntity<News> actual = controller.getById(UUID.randomUUID());
+
+        assertEquals(404, actual.getStatusCode().value() );
+    }
+
+    @Test
+    public void shouldReturnNews() {
+        repo = mock(NewsRepository.class);
+        controller = new NewsController(repo, newsService);
+
+        when(repo.findById(any())).thenReturn(Optional.of(new News()));
+
+        ResponseEntity<News> actual = controller.getById(UUID.randomUUID());
+
+        assertEquals(200, actual.getStatusCode().value() );
+        assertNotNull(actual.getBody());
+    }
 }
