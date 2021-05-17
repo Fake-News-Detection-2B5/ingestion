206394fb0b9781362c50305ad1b51dc99db8738e
Tiganasu Cristina-Ecaterina <79219863+TiganasuCristinaEcaterina@users.noreply.github.com>
2021-05-17 23:58:43 +0300

    Updated NBCScrapper

--- src/main/java/com/ingestion/management/business/NBCNews.java
+++ src/main/java/com/ingestion/management/business/NBCNews.java
@@ -18,22 +18,35 @@ public class NBCNews {
     public JSONObject scrapPageContent(String link) throws IOException {
         StringBuilder newsBody = new StringBuilder();
         JSONObject newsDetails = new JSONObject();
-        Document doc = Jsoup.connect(link).userAgent("Mozilla").get();
+        Document doc = Jsoup.connect(link).userAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36").get();
         String newsTitle = doc.title();
         String newsAuthor = "Unknown";
         String newsDate = "Unknown";
         String newsThumbnail = "Unknown";
 
         Elements mainBlock = doc.getElementsByTag("article").attr("class", "article-body");
-        for (Element e : mainBlock) {
-            Elements author_elem = e.getElementsByTag("section").attr("class", "mb7");
-            if (author_elem.size() > 0) {
-                Element a = author_elem.get(0);
-                if (a.getElementsByTag("div").attr("class", "founders-cond").contains("By")) {
-                    if (a.getElementsByTag("div").attr("class", "founders-cond").text().split("By")[1].length() > 0)
-                        newsAuthor = a.getElementsByTag("div").attr("class", "founders-cond").text().split("By")[1];
+
+
+        if (doc.getElementsByClass("mb7").size() > 0) {
+            if (doc.getElementsByClass("mb7").get(0).getElementsByAttribute("data-test").text().split("By").length > 0) {
+                if (doc.getElementsByClass("mb7").get(0).getElementsByAttribute("data-test").text().contains("By")) {
+                    String newsAuthorBlock = doc.getElementsByClass("mb7").get(0).getElementsByAttribute("data-test").text().split("By")[1];
+                    newsAuthor = newsAuthorBlock;
                 }
             }
+        }
+
+
+        if (doc.getElementsByClass("article-hero__main-image").size() > 0) {
+            if (doc.getElementsByClass("article-hero__main-image").get(0).getElementsByTag("img").size() > 0) {
+                String newsThumbnailblock = doc.getElementsByClass("article-hero__main-image").get(0).getElementsByTag("img").get(0).attr("src");
+                newsThumbnail = newsThumbnailblock;
+            }
+        }
+
+        String newsDateBlock = doc.getElementsByTag("time").attr("content");
+        if (newsDateBlock.length() > 0) {
+            newsDate = newsDateBlock;
         }
 
         for (Element e : mainBlock) {
@@ -51,7 +64,10 @@ public class NBCNews {
             newsDetails.put("title", newsTitle);
             newsDetails.put("author", newsAuthor);
             newsDetails.put("url", link);
-            newsDetails.put("content", JSONValue.escape(newsBody.toString()));
+            newsDetails.put("description", JSONValue.escape(newsBody.toString()));
+            newsDetails.put("postDate", newsDate);
+            newsDetails.put("thumbnail", newsThumbnail);
+
             urlList.add(link);
             return newsDetails;
         } else
@@ -59,7 +75,7 @@ public class NBCNews {
     }
 
 
-    public String scrapMainPage() throws IOException {
+    public String scrapMainPage() throws IOException, InterruptedException {
         String[] newsCategories = {"", "world", "politics", "business", "health", "entertainment"};
         JSONArray newsList = new JSONArray();
         JSONObject tempNews;
@@ -67,12 +83,13 @@ public class NBCNews {
         for (String category : newsCategories) {
             String url = "https://www.nbcnews.com/" + category;
 
-            Document doc = Jsoup.connect(url).userAgent("Mozilla").get();
+            Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36").get();
             Elements mainLinks = doc.select("a[href]");
             for (Element e : mainLinks) {
                 String tempUrl = e.attr("href");
                 if (tempUrl.matches("(.*)/(.*)[(0-9)]+")) {
                     if (!tempUrl.contains("/video/")) {
+                        Thread.sleep(   50);
                         tempNews = scrapPageContent(tempUrl);
                         if (tempNews != null)
                             newsList.add(tempNews);
